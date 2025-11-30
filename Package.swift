24777d4bcbe1a8afd19580f9f816dbd03b86f6ad
Nick Arner <nicholasarner@gmail.com>
2025-11-30 09:10:32 -0800

    package url

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "chroma_swiftFFI",
-            url: "https://github.com/chroma-core/chroma-swift/releases/download/untagged-9bb63757553b689712e7/chroma_swift_framework.xcframework.zip",
+            url: "https://github.com/chroma-core/chroma-swift/releases/download/1.0.1/chroma_swift_framework.xcframework.zip",
             checksum: "d438e2d46544947c59261fda17b2640c9b452e3afbdcdd3d1b2c28bee82c3d51"
         )
     ]
